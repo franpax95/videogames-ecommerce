3373c773e06a74a4ca9eab7677af0bd16a27af22
franpax95 <fcojnavarrowork@gmail.com>
2024-11-22 18:35:29 +0100

    feat(sidebar): update footer to show auth options

--- src/components/shop/sidebar/app-sidebar-footer.tsx
+++ src/components/shop/sidebar/app-sidebar-footer.tsx
@@ -1,26 +1,49 @@
+'use client';
+
 import Link from 'next/link';
 import { SidebarFooter } from '@/components/ui/sidebar';
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
 import { Locale } from '@/types/locale';
+import { useSession } from '@/hooks/use-session';
+import { ShoppingCart, User } from 'lucide-react';
 
 export function AppSidebarFooter({ lang }: { lang: Locale }) {
+  const { user } = useSession();
+
   return (
     <SidebarFooter>
-      <div className="flex w-full justify-between space-x-2 p-4">
-        <Link
-          href={`/${lang}/login`}
-          className={`${buttonVariants({
-            variant: 'outline',
-            className: 'text-sm'
-          })} flex-1`}
-        >
-          Sign In
-        </Link>
+      {user ? (
+        <div className="flex w-full justify-between space-x-2 p-4">
+          <Link
+            href={`/${lang}/account`}
+            className={`${buttonVariants({
+              variant: 'outline',
+              size: 'lg'
+            })} flex-1`}
+          >
+            <User />
+          </Link>
 
-        <Link href={`/${lang}/register`} className={`${buttonVariants({})} flex-1`}>
-          Sign Up
-        </Link>
-      </div>
+          <Button className={`${buttonVariants({ variant: 'default', size: 'lg' })} flex-1`}>
+            <ShoppingCart />
+          </Button>
+        </div>
+      ) : (
+        <div className="flex w-full justify-between space-x-2 p-4">
+          <Link
+            href={`/${lang}/login`}
+            className={`${buttonVariants({
+              variant: 'outline'
+            })} flex-1`}
+          >
+            Sign In
+          </Link>
+
+          <Link href={`/${lang}/register`} className={`${buttonVariants({})} flex-1`}>
+            Sign Up
+          </Link>
+        </div>
+      )}
     </SidebarFooter>
   );
 }
