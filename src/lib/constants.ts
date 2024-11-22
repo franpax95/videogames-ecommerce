641643609f738ea745da995dbbe3a4bebb63bb43
franpax95 <fcojnavarrowork@gmail.com>
2024-11-22 18:21:42 +0100

    feat(constants): add session cookie id & auth error state

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export enum AUTH_ERROR {
+  INVALID_CREDENTIALS = '__INVALID_CREDENTIALS__',
+  INCORRECT_CREDENTIALS = '__INCORRECT_CREDENTIALS__',
+  SERVER_ERROR = '__SERVER_ERROR__'
+}
+
+export const sessionCookieId = 'app-session';
