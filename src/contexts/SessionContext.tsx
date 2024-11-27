5fe3ee4a4e32fce74b74f4e6ba3c253dd054d127
franpax95 <fcojnavarrowork@gmail.com>
2024-11-27 18:11:12 +0100

    refactor(sessionContext): refactor methods to handle errors; add change password method

--- src/contexts/SessionContext.tsx
+++ src/contexts/SessionContext.tsx
@@ -3,16 +3,21 @@
 import React, { createContext, useState, ReactNode } from 'react';
 import { User } from '@/types/user';
 import { getSession } from '@/lib/session';
-import { login, logout, register } from '@/app/api/auth';
+import { changePassword, login, logout, register } from '@/app/api/auth';
 import { Session } from '@/types/session';
+import { useRouter } from 'next/navigation';
+import { ApiError } from '@/lib/api-error';
+import { SerializedError } from '@/types/serialized-error';
 
 interface SessionContextType {
+  loading: boolean;
   user: User | null;
   token: string | null;
+  updateSession: () => Promise<void>;
   register: (formData: FormData) => Promise<void>;
   login: (formData: FormData) => Promise<void>;
-  logout: () => Promise<void>;
-  loading: boolean;
+  logout: (redirectUrl?: string) => Promise<void>;
+  changePassword: (formData: FormData) => Promise<void>;
 }
 
 interface SessionProviderProps {
@@ -23,46 +28,60 @@ interface SessionProviderProps {
 export const SessionContext = createContext<SessionContextType | undefined>(undefined);
 
 export const SessionProvider: React.FC<SessionProviderProps> = ({ initialSession, children }) => {
+  const router = useRouter();
+
   const [user, setUser] = useState<User | null>(initialSession?.user ?? null);
   const [token, setToken] = useState<string | null>(initialSession?.token ?? '');
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleRegister = async (formData: FormData) => {
-    try {
-      setLoading(true);
-      await register(formData);
-      const session = await getSession();
-      if (session) {
-        setUser(session.user);
-        setToken(session.token);
-      }
-    } catch (error) {
-      throw error;
-    } finally {
+  const updateSession = async () => {
+    await getSession().then((session) => {
+      setUser(session?.user ?? null);
+      setToken(session?.token ?? '');
+    });
+  };
+
+  const authCall = async (
+    action: (formData: FormData) => Promise<void | SerializedError>,
+    formData: FormData
+  ) => {
+    setLoading(true);
+
+    const error = await action(formData);
+    if (error) {
       setLoading(false);
+      throw new ApiError(error);
     }
+
+    const session = await getSession();
+    if (session) {
+      setUser(session.user);
+      setToken(session.token);
+    }
+
+    setLoading(false);
+  };
+
+  const handleRegister = async (formData: FormData) => {
+    return authCall(register, formData);
   };
 
   const handleLogin = async (formData: FormData) => {
-    try {
-      setLoading(true);
-      await login(formData);
-      const session = await getSession();
-      if (session) {
-        setUser(session.user);
-        setToken(session.token);
-      }
-    } catch (error) {
-      throw error;
-    } finally {
-      setLoading(false);
-    }
+    return authCall(login, formData);
   };
 
-  const handleLogout = async () => {
+  const handleLogout = async (redirectUrl?: string) => {
     await logout();
     setUser(null);
     setToken(null);
+
+    if (redirectUrl) {
+      router.push(redirectUrl);
+    }
+  };
+
+  const handleChangePassword = async (formData: FormData) => {
+    return authCall(changePassword, formData);
   };
 
   return (
@@ -71,9 +90,11 @@ export const SessionProvider: React.FC<SessionProviderProps> = ({ initialSession
         loading,
         user,
         token,
+        updateSession,
         register: handleRegister,
         login: handleLogin,
-        logout: handleLogout
+        logout: handleLogout,
+        changePassword: handleChangePassword
       }}
     >
       {children}
