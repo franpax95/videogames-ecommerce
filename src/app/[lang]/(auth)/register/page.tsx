cd60b567a1362c9776963aabd5519dc06276bc95
franpax95 <fcojnavarrowork@gmail.com>
2024-11-29 16:31:32 +0100

    refactor(registerPage): add translations provider to register page

--- src/app/[lang]/(auth)/register/page.tsx
+++ src/app/[lang]/(auth)/register/page.tsx
@@ -3,22 +3,49 @@ import { getLocale } from '@/lib/get-locale';
 import { getLocalizedContent } from '@/app/api/get-localized-content';
 import { LocalizedContent } from '@/types/localized-content';
 import { RegisterForm } from '@/components/forms/register';
+import constants from '@/lib/constants';
+import { Dictionaries } from '@/types/Dictionaries';
+import { TranslationsProvider } from '@/contexts/TranslationsContext';
+
+const { localeEndpoints } = constants;
 
 export default async function RegisterPage() {
-  const lang = getLocale();
-  const dictionary = await getLocalizedContent('register', lang)
+  const locale = getLocale();
+
+  const generalDictionaryReq = getLocalizedContent(localeEndpoints.general, locale)
     .then((content: LocalizedContent) => {
       return content;
     })
     .catch((err) => {
       console.error(err);
-      return null;
+      return {};
     });
 
+  const registerDictionaryReq = getLocalizedContent(localeEndpoints.register, locale)
+    .then((content: LocalizedContent) => {
+      return content;
+    })
+    .catch((err) => {
+      console.error(err);
+      return {} as LocalizedContent;
+    });
+
+  const [generalDictionary, dictionary] = await Promise.all([
+    generalDictionaryReq,
+    registerDictionaryReq
+  ]);
+
+  const dictionaries: Dictionaries = {
+    [localeEndpoints.general]: generalDictionary,
+    [localeEndpoints.register]: registerDictionaryReq
+  };
+
   return (
-    <div className="register">
-      <h1 className="text-xl mb-4">{dictionary?.title || 'Register'}</h1>
-      <RegisterForm lang={lang} dictionary={dictionary} />
-    </div>
+    <TranslationsProvider locale={locale} dictionaries={dictionaries}>
+      <div className="register">
+        <h1 className="text-xl mb-4">{dictionary?.title || 'Register'}</h1>
+        <RegisterForm />
+      </div>
+    </TranslationsProvider>
   );
 }
