2431c97d476855df4cb0a1f38cd64d08451c735f
franpax95 <fcojnavarrowork@gmail.com>
2024-12-03 14:18:42 +0100

    feat(accTabAccount): add translations

--- src/components/shop/acc-tab-account.tsx
+++ src/components/shop/acc-tab-account.tsx
@@ -8,30 +8,44 @@ import { TabsContent } from '@/components/ui/tabs';
 import { useSession } from '@/hooks/use-session';
 import { beautifyDate } from '@/utils';
 import { ChangePasswordDialog } from '../dialogs/change-password';
+import { useTranslations } from '@/hooks/use-translations';
+import constants from '@/lib/constants';
+
+const { localeEndpoints: i18nSections } = constants;
 
 export default function AccTabAccount() {
   const { user, logout } = useSession();
+  const {
+    dictionaries: { [i18nSections.account]: dictionary, [i18nSections.general]: generalDictionary }
+  } = useTranslations();
 
   const onLogoutClick = async () => {
     try {
       await logout('/');
-      toast.info('You logout successfully');
+      toast.info(generalDictionary?.logout_success_message || 'You logout successfully');
     } catch (error) {
       console.error(error);
-      toast.error('Something went wrong.');
+      toast.error(
+        generalDictionary?.generic_server_error_message ||
+          'Something went wrong. Please, try again later.'
+      );
     }
   };
 
   return (
     <TabsContent value="account" className="acc-tab-account">
       <div className="acc-tab-account__section">
-        <h1 className="acc-tab-account__section-title">Details</h1>
+        <h1 className="acc-tab-account__section-title">
+          {dictionary?.account_tab_details_section_title || 'Details'}
+        </h1>
 
         <div className="app-badge">
           <div className="app-badge__icon">
             <AtSign />
           </div>
-          <h2 className="app-badge__title">Email</h2>
+          <h2 className="app-badge__title">
+            {dictionary?.account_tab_details_section_email_label || 'Email'}
+          </h2>
           <p className="app-badge__subtitle">{user?.email || 'Email'}</p>
         </div>
 
@@ -39,30 +53,41 @@ export default function AccTabAccount() {
           <div className="app-badge__icon">
             <Calendar1 />
           </div>
-          <h2 className="app-badge__title">Member Since</h2>
+          <h2 className="app-badge__title">
+            {dictionary?.account_tab_details_section_created_at_label || 'Member Since'}
+          </h2>
           <p className="app-badge__subtitle">{`${beautifyDate(user?.createdAt ?? '')}`}</p>
         </div>
       </div>
 
       <div className="acc-tab-account__section">
-        <h1 className="acc-tab-account__section-title">Security</h1>
+        <h1 className="acc-tab-account__section-title">
+          {dictionary?.account_tab_security_section_title || 'Security'}
+        </h1>
 
         <div className="acc-tab-account__option">
-          <h2 className="acc-tab-account__option-name">Password</h2>
+          <h2 className="acc-tab-account__option-name">
+            {dictionary?.account_tab_security_section_password_label || 'Password'}
+          </h2>
           <ChangePasswordDialog />
         </div>
       </div>
 
       <div className="acc-tab-account__section">
-        <h1 className="acc-tab-account__section-title">Preferences</h1>
+        <h1 className="acc-tab-account__section-title">
+          {dictionary?.account_tab_preferences_section_title || 'Preferences'}
+        </h1>
 
         <div className="app-badge app-badge--interactive">
           <div className="app-badge__icon">
             <Mail />
           </div>
-          <h2 className="app-badge__title">Newsletter</h2>
+          <h2 className="app-badge__title">
+            {dictionary?.account_tab_preferences_section_newsletter_label || 'Newsletter'}
+          </h2>
           <p className="app-badge__subtitle">
-            Subscribe to our newsletter for product updates and promotions.
+            {dictionary?.account_tab_preferences_section_newsletter_content ||
+              'Subscribe to our newsletter for product updates and promotions.'}
           </p>
         </div>
 
@@ -70,14 +95,19 @@ export default function AccTabAccount() {
           <div className="app-badge__icon">
             <Bell />
           </div>
-          <h2 className="app-badge__title">Offers & Updates</h2>
+          <h2 className="app-badge__title">
+            {dictionary?.account_tab_preferences_section_offers_label || 'Offers & Updates'}
+          </h2>
           <p className="app-badge__subtitle">
-            Receive news and offers from Game World and partners.
+            {dictionary?.account_tab_preferences_section_offers_content ||
+              'Receive news and offers from Game World and partners.'}
           </p>
         </div>
 
         <div className="acc-tab-account__option">
-          <h2 className="acc-tab-account__option-name">Data Sharing</h2>
+          <h2 className="acc-tab-account__option-name">
+            {dictionary?.account_tab_preferences_section_data_sharing_label || 'Data Sharing'}
+          </h2>
           <Switch className="data-[state=unchecked]:bg-slate-600" />
         </div>
       </div>
@@ -88,7 +118,7 @@ export default function AccTabAccount() {
           variant="destructive"
           onClick={onLogoutClick}
         >
-          Log out
+          {dictionary?.account_tab_logout_button || 'Log out'}
         </Button>
       </div>
     </TabsContent>
